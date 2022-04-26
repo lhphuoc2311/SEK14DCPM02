29a3fc9a9af9153d1bcb75d835f309130420d1ef
lhphuoc2311 <phuoc2311@gmail.com>
2022-04-26 11:09:58 +0700

    tai lieu ngay 26/4/2022

---
+++ src/main/java/com/cgm/buoi5/A.java
@@ -0,0 +1,16 @@
+/*
+*  created date: Apr 26, 2022
+*  author: cgm
+*/
+package com.cgm.buoi5;
+
+public class A {
+
+    public void method(B b){
+
+        //Dependency
+        B refObject = b;
+
+    }
+    
+}
---
+++ src/main/java/com/cgm/buoi5/B.java
@@ -0,0 +1,9 @@
+/*
+*  created date: Apr 26, 2022
+*  author: cgm
+*/
+package com.cgm.buoi5;
+
+public class B {
+    
+}
